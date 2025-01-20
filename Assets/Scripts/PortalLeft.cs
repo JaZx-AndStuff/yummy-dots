ae77bd8057aaecaf33b0a7ab3cc61b678ecf9fb8
DESKTOP-TKBJCVR\Frank <frankiegallagher1176@icloud.com>
2025-01-20 09:16:35 +0000

    added portals

---
+++ Assets/Scripts/PortalLeft.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PortalLeft : MonoBehaviour
+{
+    [SerializeField] Transform transform;
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (!collision.CompareTag("Player"))
+        {
+            return;
+        }
+        collision.transform.Translate(transform.position);
+    }
+}
