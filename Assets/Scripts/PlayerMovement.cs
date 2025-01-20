c780502157dc12c2b7840551f291b50a1fd59f7b
DESKTOP-TKBJCVR\Frank <frankiegallagher1176@icloud.com>
2025-01-20 02:11:22 +0000

    Player input & tilemap

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -1,18 +1,25 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class PlayerMovement : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    [SerializeField] Rigidbody2D rb;
+    Vector2 move;
+   
+    void OnMove(InputValue value)
     {
-        
+        move = value.Get<Vector2>();
     }
 
-    // Update is called once per frame
-    void Update()
+    private void Update()
     {
-        
+        Run();
+    }
+
+    private void Run()
+    {
+        rb.velocity = move * 4;
     }
 }
